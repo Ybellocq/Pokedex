b7745e1b5e794ccd4121e0d1e9b59ffebae82994
Ybellocq <134070832+Ybellocq@users.noreply.github.com>
2025-02-13 12:13:12 +0100

    add id pokemon

--- PokeNative/app/index.tsx
+++ PokeNative/app/index.tsx
@@ -1,14 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
-    <View style={styles.container}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about">A propos</Link>
+      <Link href={{pathname: '/pokemon/[id]', params: {id: 3}}}>Pokemon 3</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {backgroundColor: '#FF0000', padding: 24}
-})
+const styles = {
+  container: {
+    backgroundColor: "#FF0000",
+    padding: 24,
+  },
+};
